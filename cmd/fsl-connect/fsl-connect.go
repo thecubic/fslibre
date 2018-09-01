de27185b777017b5ba9049cecce037a6b196e166
Dave Carlson <thecubic@thecubic.net>
2018-09-01 14:37:12 -0700

    major update w/history command support

--- cmd/fsl-connect/fsl-connect.go
+++ cmd/fsl-connect/fsl-connect.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	var err error
-	edevs := hid.Enumerate(fslibre.UsbDevice, fslibre.UsbDevice)
+	edevs := hid.Enumerate(fslibre.UsbVendor, fslibre.UsbDevice)
+	fmt.Fprintf(os.Stderr, "hm: %v\n", edevs)
 	if len(edevs) == 0 {
 		fmt.Fprintf(os.Stderr, "no Freestyle Libre Reader Devices found!\n")
 		os.Exit(1)
@@ -27,6 +28,84 @@ func main() {
 		if err != nil {
 			log.Printf("error initializing LibreReader: %v", err)
 		}
+
+		var sn string
+		sn, err = lbr.SerialNumber()
+		if err != nil {
+			log.Printf("error retrieving serial number: %v", err)
+		} else {
+			fmt.Printf("serial number: %v\n", sn)
+		}
+
+		var swv string
+		swv, err = lbr.SwVersion()
+		if err != nil {
+			log.Printf("error retrieving swversion: %v", err)
+		} else {
+			fmt.Printf("swversion: %v\n", swv)
+		}
+
+		//
+		// var arr string
+		// arr, err = lbr.Arresult()
+		// if err != nil {
+		// 	log.Printf("error retrieving arr: %v", err)
+		// } else {
+		// 	log.Printf("arr: %v", arr)
+		// }
+
+		// var dt string
+		// dt, err = lbr.DateTime()
+		// if err != nil {
+		// 	log.Printf("error retrieving dt: %v", err)
+		// } else {
+		// 	log.Printf("dt: %v", dt)
+		// }
+
+		hst, err := lbr.History()
+		if err != nil {
+			log.Printf("error retrieving history: %v", err)
+		} else {
+			fmt.Printf("history: %v records\n", len(hst.Records))
+			fmt.Printf("latest record: %v\n", hst.Records[0])
+		}
+
+		// arr, err := lbr.ArrHistory()
+		// if err != nil {
+		// 	log.Printf("error retrieving arrhistory: %v", err)
+		// } else {
+		// 	fmt.Printf("arrhistory: %v records\n", len(arr.Records))
+		// 	fmt.Printf("latest record: %v\n", arr.Records[0])
+		// }
+
+		var dbrnum int
+		dbrnum, err = lbr.Dbrnum()
+		if err != nil {
+			log.Printf("error retrieving db record number: %v", err)
+		} else {
+			fmt.Printf("db record number: %v\n", dbrnum)
+		}
+
+		// var ptname string
+		// ptname, err = lbr.PatientName()
+		// if err != nil {
+		// 	log.Printf("error retrieving patient name: %v", err)
+		// } else {
+		// 	log.Printf("patient name: %v", ptname)
+		// }
+		//
+		// var ptid string
+		// ptid, err = lbr.PatientId()
+		// if err != nil {
+		// 	log.Printf("error retrieving patient id: %v", err)
+		// } else {
+		// 	log.Printf("patient id: %v", ptid)
+		// }
+		err = lbr.Close()
+		if err != nil {
+			log.Printf("error during close: %v", err)
+		}
+
 	}
 	log.Print("finished hidtest")
 }
--- librereader.go
+++ librereader.go
@@ -2,8 +2,12 @@ package fslibre
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/karalabe/hid"
 	"log"
+	// "os"
+	"strconv"
+	"strings"
 )
 
 type LibreReader struct {
@@ -14,6 +18,26 @@ type LibreReader struct {
 	version    string
 }
 
+type libreresponse struct {
+	text    string
+	ok      bool
+	cksm    uint64
+	cksm_ok bool
+}
+
+// type historyrecord struct {
+// 	sequence int
+// }
+
+type HistoryRecords struct {
+	Records [][16]int
+}
+
+const UsbVendor uint16 = 0x1a61
+const UsbDevice uint16 = 0x3650
+
+// PUBLIC API: DEVICE MANAGEMENT
+
 func New(di *hid.DeviceInfo) LibreReader {
 	// make it
 	var li LibreReader
@@ -21,8 +45,142 @@ func New(di *hid.DeviceInfo) LibreReader {
 	return li
 }
 
-const UsbVendor uint16 = 0x1a61
-const UsbDevice uint16 = 0x3650
+func (lbr *LibreReader) Init() error {
+	log.Println("initializing LibreReader")
+	err := lbr.handshake()
+	return err
+}
+
+func (lbr *LibreReader) Open() error {
+	log.Println("opening LibreReader")
+	device, err := lbr.deviceinfo.Open()
+	lbr.device = device
+	return err
+}
+
+func (lbr *LibreReader) Close() error {
+	err := lbr.device.Close()
+	log.Println("closed LibreReader")
+	return err
+}
+
+// PUBLIC API: DEVICE COMMANDS
+
+func (lbr *LibreReader) SerialNumber() (string, error) {
+	resps, err := lbr.text_command("$sn?")
+	if resps.ok {
+		return resps.text, err
+	} else if err != nil {
+		return "", err
+	} else {
+		return "", fmt.Errorf("command failure")
+	}
+}
+
+func (lbr *LibreReader) SwVersion() (string, error) {
+	resps, err := lbr.text_command("$swver?")
+	if resps.ok {
+		return resps.text, err
+	}
+	return "", fmt.Errorf("command failure")
+}
+
+func (lbr *LibreReader) DateTime() (string, error) {
+	date, err := lbr.text_command("$date?")
+	if err != nil {
+		return "", err
+	}
+
+	time, err := lbr.text_command("$time?")
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("date: %v\ntime: %v\n", date.text, time.text)
+	return "", err
+}
+
+func (lbr *LibreReader) History() (*HistoryRecords, error) {
+	var err error
+	err = lbr.send_text_command("$history?")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := lbr.history_recv()
+	return resp, err
+}
+
+// TODO: not the same as history
+// func (lbr *LibreReader) ArrHistory() (*HistoryRecords, error) {
+// 	var err error
+// 	err = lbr.send_text_command("$arresult?")
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	resp, err := lbr.history_recv()
+// 	return resp, err
+// }
+
+// func (lbr *LibreReader) Arresult() (string, error) {
+// 	resps, err := lbr.raw_text_command("$arresult?")
+// 	return resps, err
+// }
+
+func (lbr *LibreReader) Dbrnum() (int, error) {
+	resps, err := lbr.text_command("$dbrnum?")
+	if err != nil {
+		return -1, err
+	}
+	if resps.ok {
+		if resps.text[:19] == "DB Record Number = " {
+			return strconv.Atoi(resps.text[19:])
+		} else {
+			return -1, fmt.Errorf("response unknown")
+		}
+	}
+	return -1, fmt.Errorf("command failure")
+}
+
+// TODO: broken, probably because they are not set and thus special
+
+// func (lbr *LibreReader) PatientName() (string, error) {
+// 	resps, err := lbr.text_command("$ptname?")
+// 	if resps.ok {
+// 		return resps.text, err
+// 	}
+// 	return "", fmt.Errorf("command failure")
+// }
+//
+// func (lbr *LibreReader) PatientId() (string, error) {
+// 	resps, err := lbr.text_command("$ptid?")
+// 	if resps.ok {
+// 		return resps.text, err
+// 	}
+// 	return "", fmt.Errorf("command failure")
+// }
+
+// PRIVATE
+
+func (lbr *LibreReader) send_text_command(cmd string) error {
+	packet := bytes.Repeat([]byte{0x00}, 65)
+	packet[1] = 0x60
+	packet[2] = byte(len(cmd))
+	copy(packet[3:], cmd)
+	return lbr.send(packet)
+}
+
+// func (lbr *LibreReader) raw_text_command(cmd string) (string, error) {
+// 	var err error
+// 	err = lbr.send_text_command(cmd)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	resp, err := lbr.multi_recv()
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	return string(resp), err
+// }
 
 func packetize(rpkt []byte) (byte, []byte) {
 	var length int
@@ -45,36 +203,22 @@ func (lbr *LibreReader) handshake() error {
 		pktType, payload = packetize(rbuf)
 		if pktType == 0x06 {
 			lbr.serial = string(payload[:])
-			log.Printf("serial: %v", lbr.serial)
 		}
 		if pktType == 53 {
 			lbr.version = string(payload[:])
-			log.Printf("version: %v", lbr.version)
 		}
-		log.Printf("completed rpc %v -> %v (%v)", cmd, pktType, len(payload))
 	}
-	log.Print("handshake succeeded")
 	return err
 }
 
 func (lbr *LibreReader) send(wbuf []byte) error {
-	wcnt, err := lbr.device.Write(wbuf)
-	if err != nil {
-		log.Printf("error writing: %v", err)
-	} else {
-		log.Printf("wrote: %v", wcnt)
-	}
+	_, err := lbr.device.Write(wbuf)
 	return err
 }
 
 func (lbr *LibreReader) recv() ([]byte, error) {
 	rbuf := bytes.Repeat([]byte{0x00}, 64)
-	rcnt, err := lbr.device.Read(rbuf)
-	if err != nil {
-		log.Printf("error reading: %v", err)
-	} else {
-		log.Printf("read: %v", rcnt)
-	}
+	_, err := lbr.device.Read(rbuf)
 	return rbuf, err
 }
 
@@ -89,30 +233,136 @@ func (lbr *LibreReader) rpc(cmd byte) ([]byte, error) {
 		return rbuf, err
 	}
 	rbuf, err = lbr.recv()
-	if err != nil {
-		log.Printf("RPC %v failed", cmd)
-	} else {
-		log.Printf("<- %v", rbuf)
-		log.Printf("RPC %v succeeded", cmd)
-	}
 	return rbuf, err
 }
 
-func (lbr *LibreReader) Init() error {
-	log.Println("initializing LibreReader")
-	err := lbr.handshake()
-	return err
+func (lbr *LibreReader) history_recv() (*HistoryRecords, error) {
+	var err error
+	var nrecords int
+	// 2000 should be enough
+	var records [2000][16]int
+	var c_records_cksum uint64
+	var records_cksum uint64
+	var c_full_cksum uint64
+	var full_cksum uint64
+
+	recordidx := 0
+	failure := true
+	rdone := false
+
+	c_records_cksum = 0
+	c_full_cksum = 0
+
+	for !rdone {
+		pkt, err := lbr.recv()
+		if err != nil {
+			return nil, err
+		}
+		// pktType, izepkt := packetize(pkt)
+		resplen := int(pkt[1])
+		resptext := string(pkt[2 : 2+resplen])
+		var pktcksum uint64
+		pktcksum = 0
+		for _, chr := range pkt[2 : 2+resplen] {
+			pktcksum += uint64(chr)
+		}
+		respfields := strings.Split(resptext, "\r\n")
+		nfields := len(respfields)
+		if nfields == 2 {
+			var data [16]int
+			for i, datum := range strings.Split(respfields[0], ",") {
+				data[i], err = strconv.Atoi(datum)
+			}
+			records[recordidx] = data
+			recordidx++
+			c_records_cksum += pktcksum
+			c_full_cksum += pktcksum
+		} else if nfields == 4 {
+			// only add the first field
+			for _, chr := range pkt[2 : 4+len(respfields[0])] {
+				c_full_cksum += uint64(chr)
+			}
+
+			endinfo := strings.Split(respfields[0], ",")
+			nrecords, err = strconv.Atoi(endinfo[0])
+			records_cksum, err = strconv.ParseUint(endinfo[1], 16, 32)
+
+			if respfields[1][:5] == "CKSM:" {
+				full_cksum, err = strconv.ParseUint(respfields[1][5:], 16, 32)
+			}
+			if respfields[2] == "CMD OK" {
+				failure = false
+			}
+			rdone = true
+		}
+	}
+	if failure {
+		return nil, fmt.Errorf("general protocol read failure")
+	}
+	if nrecords != recordidx {
+		return nil, fmt.Errorf("number of records mismatch: %v received, %v expected", recordidx, nrecords)
+	}
+	if c_full_cksum != full_cksum {
+		return nil, fmt.Errorf("checksum mismatch: %v received, %v calculated", full_cksum, c_full_cksum)
+	}
+	if c_records_cksum != records_cksum {
+		return nil, fmt.Errorf("records checksum mismatch: %v received, %v calculated", records_cksum, c_records_cksum)
+	}
+	return &HistoryRecords{records[:nrecords]}, err
 }
 
-func (lbr *LibreReader) Open() error {
-	log.Println("opening LibreReader")
-	device, err := lbr.deviceinfo.Open()
-	lbr.device = device
-	return err
-}
+func (lbr *LibreReader) text_command(cmd string) (*libreresponse, error) {
+	var err error
 
-func (lbr *LibreReader) close() error {
-	err := lbr.device.Close()
-	log.Println("closed LibreReader")
-	return err
+	err = lbr.send_text_command(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := lbr.recv()
+	if err != nil {
+		return nil, err
+	}
+
+	if resp[0] != 0x60 {
+		log.Printf("response not text-response (%v)!", resp[0])
+	}
+
+	var cksum uint64
+
+	resplen := int(resp[1])
+	resptext := string(resp[2 : 2+resplen])
+	respfields := strings.Split(resptext, "\r\n")
+	respstruct := new(libreresponse)
+	respstruct.text = respfields[0]
+
+	if len(respfields) > 1 {
+		cksum = 0
+		for _, chr := range []byte(respfields[0]) {
+			cksum += uint64(chr)
+		}
+		cksum += uint64('\r')
+		cksum += uint64('\n')
+		ckv := strings.Split(respfields[1], ":")
+		if len(ckv) > 1 {
+			rcksum, _ := strconv.ParseUint(ckv[1], 16, 32)
+			respstruct.cksm = rcksum
+			respstruct.cksm_ok = rcksum == cksum
+		} else {
+			for _, rstr := range respfields[1:] {
+				log.Printf("line: %v\n", rstr)
+			}
+		}
+	}
+
+	if len(respfields) > 2 {
+		if respfields[2] == "CMD OK" {
+			respstruct.ok = true
+		} else {
+			respstruct.ok = false
+			// err = fmt.Errorf("checkum mismatch: %v (recv) vs %v (calc)", rcksum, cksum)
+		}
+	}
+
+	return respstruct, err
 }
